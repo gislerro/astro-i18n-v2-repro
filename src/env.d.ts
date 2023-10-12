55e384533c05a8961e4c656e8bdf1447c271ae81
Robin Gisler <63500142+gislerro@users.noreply.github.com>
2023-10-12 10:11:34 +0000

    common isn't accessible

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,7 +7,7 @@ type SecondaryLocale = "de"
 type Locale = PrimaryLocale | SecondaryLocale
 type RouteParameters = {"/about":undefined;"/":undefined;}
 type Route = keyof RouteParameters
-type TranslationVariables = object
+type TranslationVariables = {"about":object|undefined;"example":object|undefined;}
 type Translation = keyof TranslationVariables
 type Environment = "none"|"node"|"browser"
 declare module "astro-i18n" {
